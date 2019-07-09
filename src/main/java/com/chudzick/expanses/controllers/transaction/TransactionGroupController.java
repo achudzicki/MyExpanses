031e86041b3402b3ab40c26d16b3d26970574e7c
Andrzej Chudzicki <chudzick@wszib.edu.pl>
2019-07-09 11:17:35 +0200

    Add redirect after success add new transaction group

--- src/main/java/com/chudzick/expanses/controllers/transaction/TransactionGroupController.java
+++ src/main/java/com/chudzick/expanses/controllers/transaction/TransactionGroupController.java
@@ -4,16 +4,21 @@ import com.chudzick.expanses.beans.TransactionGroupBean;
 import com.chudzick.expanses.builders.NotificationMessageListBuilder;
 import com.chudzick.expanses.domain.expanses.TransactionGroupDto;
 import com.chudzick.expanses.domain.responses.NotificationMessagesBean;
+import com.chudzick.expanses.domain.responses.SimpleNotificationMsg;
 import com.chudzick.expanses.services.transactions.TransactionGroupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "transaction/group")
@@ -30,29 +35,8 @@ public class TransactionGroupController {
 
 
     @GetMapping
-    public String transactionGroupMain(Model model) {
-        setUpMainPageBeans();
-
-        model.addAttribute("transactionGroupBean", transactionGroupBean);
-        model.addAttribute("transactionGroupDto", new TransactionGroupDto());
-        return ""; //TODO RETURN VIEW OF MAIN TRANSACTION GROUP
-    }
-
-    @PostMapping
-    public String addNewTransactionGroup(@Valid TransactionGroupDto transactionGroupDto, Model model, BindingResult bindingResult) {
-
-        if (bindingResult.hasErrors()) {
-            return ""; //TODO RETURN VIEW OF MAIN TRANSACTION GROUP
-        }
-
-        setUpMainPageBeans();
-        transactionGroupService.addNewTransactionGroup(transactionGroupDto);
-
-        notificationMessagesBean.setNotificationsMessages(
-                new NotificationMessageListBuilder()
-                        .withSuccessNotification("{transaction.group.succes.add.message}")
-                        .getNotificationList()
-        );
+    public String transactionGroupMain(@ModelAttribute("notifications") List<SimpleNotificationMsg> notifications, Model model) {
+        setUpMainPageBeans(notifications);
 
         model.addAttribute("transactionGroupBean", transactionGroupBean);
         model.addAttribute("notificationMessagesBean", notificationMessagesBean);
@@ -60,7 +44,28 @@ public class TransactionGroupController {
         return ""; //TODO RETURN VIEW OF MAIN TRANSACTION GROUP
     }
 
-    private void setUpMainPageBeans() {
+    @PostMapping(value = "/add")
+    public String addNewTransactionGroup(@Valid TransactionGroupDto transactionGroupDto, Model model, BindingResult bindingResult,
+                                         RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+            return ""; //TODO RETURN VIEW OF MAIN TRANSACTION GROUP
+        }
+        transactionGroupService.addNewTransactionGroup(transactionGroupDto);
+
+        redirectAttributes.addFlashAttribute("notifications", new NotificationMessageListBuilder()
+                .withSuccessNotification("{transaction.group.succes.add.message}")
+                .getNotificationList());
+        return "redirect:/transaction/group";
+    }
+
+    @ModelAttribute(name = "notifications")
+    public List<SimpleNotificationMsg> defaultNotificationList() {
+        return new ArrayList<>();
+    }
+
+    private void setUpMainPageBeans(List<SimpleNotificationMsg> notifications) {
+        notificationMessagesBean.setNotificationsMessages(notifications);
         transactionGroupBean.setUsersTransactionsGroups(transactionGroupService.getAllGroups());
     }
 }
