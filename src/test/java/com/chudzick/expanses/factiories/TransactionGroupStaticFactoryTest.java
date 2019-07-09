d41f21d264ee22f9d5bdfa6b00c0063edcb00982
Andrzej Chudzicki <chudzick@wszib.edu.pl>
2019-07-09 11:28:55 +0200

    Add transactionGroupfactory test, add main layout to transaction groups

---
+++ src/test/java/com/chudzick/expanses/factiories/TransactionGroupStaticFactoryTest.java
@@ -0,0 +1,28 @@
+package com.chudzick.expanses.factiories;
+
+import com.chudzick.expanses.TestUserSupplier;
+import com.chudzick.expanses.domain.expanses.TransactionGroup;
+import com.chudzick.expanses.domain.expanses.TransactionGroupDto;
+import com.chudzick.expanses.domain.users.AppUser;
+import com.chudzick.expanses.factories.AppUserStaticFactory;
+import com.chudzick.expanses.factories.TransactionGroupStaticFactory;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TransactionGroupStaticFactoryTest implements TestUserSupplier {
+
+    @Test
+    public void createFromDtoTest() {
+        TransactionGroupDto transactionGroupDto = new TransactionGroupDto();
+        transactionGroupDto.setGroupDescription("testDesc");
+        transactionGroupDto.setGorupName("testName");
+
+        AppUser currentUserMock = AppUserStaticFactory.createFromDto(prepareValidUserDto());
+
+        TransactionGroup transactionGroup = TransactionGroupStaticFactory.createFromDto(transactionGroupDto, currentUserMock);
+
+        Assert.assertEquals(transactionGroup.getAppUser(), currentUserMock);
+        Assert.assertEquals(transactionGroupDto.getGorupName(),transactionGroup.getGorupName());
+        Assert.assertEquals(transactionGroupDto.getGroupDescription(),transactionGroup.getGroupDescription());
+    }
+}
